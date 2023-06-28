7179b10b9820440563e8e4c530570455b434895a
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-06-28 09:47:45 -0600

    Set up server and database

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 
+const app = express();
+const PORT = process.env.PORT || 4001;
+const apiRouter = require('./api/api.js');
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(errorhandler());
+
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
