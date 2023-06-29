fa7be629ee6968766ed2d1fa46147c36606c0a03
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-06-29 09:11:02 -0600

    Add GET /api/series/:seriesId route

--- api/series.js
+++ api/series.js
@@ -4,6 +4,21 @@ const sqlite3 = require('sqlite3');
 const seriesRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+// Search series in the database, if it exists attach it to req object along with its ID
+seriesRouter.param('seriesId', (req, res, next, id) => {
+    db.get("SELECT * FROM Series WHERE id = $id", {$id: id}, (error, row) => {
+        if (error) {
+            next(error);
+        } else if (row) {
+            req.series = row;
+            req.id = id;
+            next();
+        } else {
+            res.status(404).send();
+        }
+    });
+});
+
 // Get all series
 seriesRouter.get('/', (req, res) => {
     db.all("SELECT * FROM Series", (error, rows) => {
@@ -13,6 +28,11 @@ seriesRouter.get('/', (req, res) => {
             res.status(200).json({series: rows});
         }
     });
+});
+
+// Get a series
+seriesRouter.get('/:seriesId', (req, res) => {
+    res.status(200).json({series: req.series});
 });
 
 module.exports = seriesRouter;
