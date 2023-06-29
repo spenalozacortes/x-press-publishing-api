f498877147a5dbb4997c512fc8cb4db9e3fd5e21
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-06-29 09:18:53 -0600

    Add POST /api/series route

--- api/series.js
+++ api/series.js
@@ -30,9 +30,35 @@ seriesRouter.get('/', (req, res) => {
     });
 });
 
-// Get a series
+// Get series
 seriesRouter.get('/:seriesId', (req, res) => {
     res.status(200).json({series: req.series});
+});
+
+// Create series
+seriesRouter.post('/', (req, res) => {
+    const { name, description } = req.body.series;
+
+    if (name && description) {
+        db.run("INSERT INTO Series (name, description) VALUES ($name, $description)", {
+            $name: name,
+            $description: description
+        }, function(error) {
+            if (error) {
+                next(error);
+            } else {
+                db.get("SELECT * FROM Series WHERE id = $id", {$id: this.lastID}, (error, row) => {
+                    if (error) {
+                        next(error);
+                    } else {
+                        res.status(201).json({series: row});
+                    }
+                });
+            }
+        });
+    } else {
+        res.status(400).send();
+    }
 });
 
 module.exports = seriesRouter;
