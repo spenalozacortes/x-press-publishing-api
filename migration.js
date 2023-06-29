6cd2284ed2c60709b1c4551522675e26a546034a
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-06-29 08:48:03 -0600

    Create Series table

--- migration.js
+++ migration.js
@@ -10,3 +10,9 @@ db.run(`CREATE TABLE IF NOT EXISTS Artist (
         is_currently_employed INTEGER NOT NULL DEFAULT 1
 )`
 );
+
+db.run(`CREATE TABLE IF NOT EXISTS Series (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        description TEXT NOT NULL
+)`);
