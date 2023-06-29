30ee4ee6ab0a16e76a276968e140419e1732cd10
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-06-29 13:26:36 -0600

    Add PUT /api/series/:seriesId/issues/:issueId

--- api/issues.js
+++ api/issues.js
@@ -4,6 +4,18 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 const issuesRouter = express.Router({mergeParams: true});
 
+// Check if issue with provided ID exists and attach ID to req object
+issuesRouter.param('issueId', (req, res, next, issueId) => {
+    db.get("SELECT * FROM Issue WHERE id = $id", {$id: issueId}, (error, row) => {
+        if (row) {
+            req.issueId = issueId;
+            next();
+        } else {
+            res.status(404).send();
+        }
+    });
+});
+
 // Check for valid inputs
 const validateData = (req, res, next) => {
     const { name, issueNumber, publicationDate, artistId, seriesId } = req.body.issue;
@@ -51,6 +63,31 @@ issuesRouter.post('/', validateData, (req, res) => {
                     next(error);
                 } else {
                     res.status(201).json({issue: row});
+                }
+            });
+        }
+    });
+});
+
+// Update issue
+issuesRouter.put('/:issueId', validateData, (req, res) => {
+    const { name, issueNumber, publicationDate, artistId, seriesId } = req.body.issue;
+    db.run("UPDATE Issue SET name = $name, issue_number = $issueNumber, publication_date = $publicationDate, artist_id = $artistId, series_id = $seriesId WHERE id = $id", {
+        $id: req.issueId,
+        $name: name,
+        $issueNumber: issueNumber,
+        $publicationDate: publicationDate,
+        $artistId: artistId,
+        $seriesId: seriesId
+    }, (error) => {
+        if (error) {
+            next(error);
+        } else {
+            db.get("SELECT * FROM Issue WHERE id = $id", {$id: req.issueId}, (error, row) => {
+                if (error) {
+                    next(error);
+                } else {
+                    res.status(200).json({issue: row});
                 }
             });
         }
